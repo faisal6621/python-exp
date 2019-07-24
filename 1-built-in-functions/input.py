99b41b5ad9ebecb923b2d432bb7ce86db6d70e6a
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-07-24 23:49:57 +0530

    added input examples

---
+++ 1-built-in-functions/input.py
@@ -0,0 +1,8 @@
+# 1 Prompting user for an input
+name = input("Enter your name\n")
+
+# 2 or no prompt
+print("Hello ", name, "! What can I do for you?", sep="")
+action = input()
+print("Thanks for having hope in me, but I am still under development and \
+could not process your command:", action)
