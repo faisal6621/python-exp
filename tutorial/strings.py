d8f32e394ce66b9069e632acbbd5175f79103ea8
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-07-26 06:50:29 +0530

    declaring single and double quoted strings

---
+++ tutorial/strings.py
@@ -0,0 +1,23 @@
+# https://docs.python.org/3/tutorial/introduction.html#strings
+# 1: String in python either enclosed in double quotes (" ")
+str1 = "\"A double quoted string\""
+
+# 2: or in single quotes (' ')
+str2 = '\'A single quoted string\''
+
+print(str1)
+print(str2)
+
+# 3: an escap character (\) is used if you are using " within double quoted
+# string or vice versa
+# - In above example we have used the escape charater
+
+# 4: but you do not need to escape ' within double quoted string and vice
+# versa
+
+str3 = "\"A ' not needed to be escaped in double quotes\""
+str4 = '\'A " not needed to be escaped in single quotes\''
+
+print(str3)
+print(str4)
+
