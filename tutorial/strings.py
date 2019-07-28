12831cbf41b9f5b7772d4fd3fe94fa9a951f822d
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-07-28 19:16:01 +0530

    more string operations

--- tutorial/strings.py
+++ tutorial/strings.py
@@ -24,10 +24,43 @@ print(str4)
 # 5: \ is not just to escape quotes, it has other meanings too. 
 # e.g., \n means a new line and if used within string will break it to two
 # lines
-str5="This string contains \new line character"
+str5 = "This string contains \new line character"
 print(str5)
 
 # 6: or treat such string as raw strings with r prefixed
-str6=r"This li\ne will not break"
+str6 = r"This li\ne will not break"
 print(str6)
+
+# 7: String literals can span multiple lines using triple quotes (""" or ''')
+# new line characters automatically included but can be prevented by using \
+str7 = """\
+    {
+      name: "John", \
+      age: 31, \
+      city: "New York"
+    }"""
+print(str7)
+
+# 8: strings can be concatenated with +
+str8 = str1 + str2
+print(str8)
+
+# 9: string literals can be concatenated putting next to each other
+str9 = "Hello " 'World'
+print(str9)
+
+# 10: strings can be repeated with *
+print(3 * "Hello World")
+
+# 11: a character is simply a string of size one. no special character types
+print(str9[0], str9[6])
+
+# 12: negative index of string will pick character from last
+print(str9[-1], str9[-7])
+
+# 13: slicing a string
+print(str9[0:5]) # from index 0 (included) to 5 (excluded)
+print(str9[6:11]) # from index 6 (included) to 11 (excluded)
+print(str9[:5]) # omitting left of : will slice from begining to 5 (excluded)
+print(str9[6:]) # omitting right of : will slice from 6 (included) to the end
 
