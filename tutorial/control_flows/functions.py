23b09a841e54fe33851f797cf294379997bc89a3
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-08-15 22:26:47 +0530

    arbitrary arguments

--- tutorial/control_flows/functions.py
+++ tutorial/control_flows/functions.py
@@ -39,8 +39,28 @@ parrot(voltage=1000000, action='VOOOOOM')  # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000)  # 2 keyword arguments
 parrot('a million', 'bereft of life', 'jump')  # 3 positional arguments
 parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+
 # but all the following calls would be invalid:
 # parrot()                     # required argument missing
 # parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
 # parrot(110, voltage=220)     # duplicate value for the same argument
 # parrot(actor='John Cleese')  # unknown keyword argument
+
+# 4.3: arbitrary arguments (varargs equivalent in Java)
+# https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop(
+    "Limburger",  # required argument
+    "It's very runny, sir.", "It's really very, VERY runny, sir.",  # arbitrary arguments
+    shopkeeper="Michael Palin", client="John Cleese", sketch="Cheese Shop Sketch"  # arbitrary arguments dictionary
+)
