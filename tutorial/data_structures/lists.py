004619af33250812c5aabdea7b529f2f38e73a89
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-08-17 10:37:49 +0530

    lists as stack and queue

--- tutorial/data_structures/lists.py
+++ tutorial/data_structures/lists.py
@@ -1,6 +1,8 @@
 # 1: lists are treated as sequences (discussed earlier in tutorial/lists.py).
 # discussing here: methods available on list object
 # https://docs.python.org/3/tutorial/datastructures.html#data-structures
+from collections import deque
+
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 
 # returns the number of times an item appeared in the list
@@ -33,3 +35,22 @@ print('default sort', fruits)
 # return the last item by removing from the list
 fruit = fruits.pop()
 print('popped', fruit, 'from', fruits)
+
+# 2: lists as stack
+# since stacks have "last-in, first-out" operation, list's append and pop helps in achieve this
+stack = [1, 2, 3]
+print('original stack', stack)
+stack.append(4)
+stack.append(5)
+print('updated stack', stack)
+popped = stack.pop()
+print('popped', popped, 'from', stack)
+
+# 3: lists as queue
+# since queues have "first-in, first-out" operation, pops from the beginning of a list is slow
+queue = deque(stack)
+print('original queue', queue)
+queue.append(5)
+print('updated queue', queue)
+popleft = queue.popleft()
+print('removed', popleft, 'from', queue)
