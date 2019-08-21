6a5fab20c0bdca202bd5291ddcfc8743eb749fe4
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-08-21 07:04:22 +0530

    adding type information for better suggestions

--- xml/xml_parsing.py
+++ xml/xml_parsing.py
@@ -1,13 +1,14 @@
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 
 
 def main():
-    tree = ElementTree.parse("sample.xml")
-    root = tree.getroot()
-    for row in root:
-        for column in row:
+    tree: ElementTree = ElementTree.parse("sample.xml")
+    root: Element = tree.getroot()
+    for row in root:  # type: Element
+        for column in row:  # type: Element
             print(column.attrib['name'], column.text, sep=' -> ', end='\t')
-        print()
+    print()
 
 
 main()
