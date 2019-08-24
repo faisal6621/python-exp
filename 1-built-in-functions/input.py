32153fc07ace47b014a29b7ee79480215669cf95
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-08-24 21:38:06 +0530

    code comments

--- 1-built-in-functions/input.py
+++ 1-built-in-functions/input.py
@@ -1,7 +1,7 @@
-# 1 Prompting user for an input
+# 1: Prompting user for an input
 name = input("Enter your name\n")
 
-# 2 or no prompt
+# 2: or no prompt
 print("Hello ", name, "! What can I do for you?", sep="")
 action = input()
 print("Thanks for having hope in me, but I am still under development and \
