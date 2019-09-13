c20a1230f98dfee30d844db2dc5165c4005d19e9
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-09-13 07:27:56 +0530

    linked list - insert node at tail

--- hackerrank/dataStructures/linkedList/linked_list_operations.py
+++ hackerrank/dataStructures/linkedList/linked_list_operations.py
@@ -1,10 +1,23 @@
-from hackerrank.dataStructures.linkedList.SinglyLinkedList import SinglyLinkedList
+from hackerrank.dataStructures.linkedList.SinglyLinkedList import SinglyLinkedList, SinglyLinkedListNode
 
 
 def printLinkedList(head):
     while head:
         print(head.data)
         head = head.next
+
+
+def insertNodeAtTail(head, data):
+    new_node = SinglyLinkedListNode(data)
+    if not head:
+        head = new_node
+        return head
+
+    tail = head
+    while tail.next:
+        tail = tail.next
+    tail.next = new_node
+    return head
 
 
 if __name__ == '__main__':
@@ -16,4 +29,7 @@ if __name__ == '__main__':
         llist_item = int(input())
         llist.insert_node(llist_item)
 
+    printLinkedList(llist.head)
+    data = input('item to add at tail: ')
+    llist.head = insertNodeAtTail(llist.head, data)
     printLinkedList(llist.head)
